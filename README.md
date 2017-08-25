efd8f4db22b021e118026c4ee8885cdac63dd530
Francisco Trucco <franciscoctrucco@gmail.com>
2017-08-25 03:11:50 -0300

    Base files with irace and arquitecture ready :)

---
+++ README.md
@@ -0,0 +1,47 @@
+# Models
+
+## Install and uninstall
+### Install
+
+You must have anaconda installed in order to install the package and the environment.
+Download anaconda from [here](https://www.continuum.io/downloads).
+
+```
+conda env create -f $ENV;
+source activate $ENV_NAME;
+```
+
+**Note**: This will create a new environment.
+
+### Uninstall
+To uninstall run:
+
+```
+source deactivate $ENV_NAME;
+conda remove --name $ENV_NAME --all;
+```
+
+## Running irace
+
+```
+./run_model.sh MODEL
+```
+
+to see which models are available:
+
+```
+./run_model.sh
+```
+
+to run all models:
+
+```
+./run_model.sh ALL
+```
+
+## Display errors
+
+If something went wrong while running irace you can run `cat *err | less` and
+`cat *out | less`. The last command shows any output of your script on
+`/dev/stdout` and the former does the same but with `/dev/stderr`.
+
---
+++ mosquitomodels/tune_params.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+declare -a models=($(ls parameters))
+
+containsElement () {
+    local e match="$1"
+    shift
+    for e; do [[ "$e" == "$match" ]] && return 0; done
+    return 1
+}
+
+function run {
+    # Please do NOT code like this
+    model=$1
+    irace -s ./.scenario -p parameters/$model -l results/$model.Rdata;
+
+    # Output results in a csv
+    program='library("irace"); load("results/'$model'.Rdata"); write.csv(getFinalElites(iraceResults, n = 0), file = "results/'$model'.csv"); q();'
+    echo $program | R --no-save > /dev/null;
+}
+
+
+# MAIN ----------------------------------------------------------------------
+
+if containsElement "$1" "${models[@]}"; then
+   model=$1;
+   run $model;
+elif [ "$1" = "all" ]; then
+    for model in "${models[@]}"; do
+        run $model;
+    done
+elif [ "$1" = "clean" ]; then
+    rm *.stdout *.stderr -f;
+else
+    # If the number of arguments is 0 or the argument is incorrect, show help message
+    echo -e "${bold}Please, specify a valid model as fisrt argument:";
+    echo -e "Models:"
+    for model in "${models[@]}"; do
+        echo -e "\t $model";
+    done
+    echo -e "";
+    echo -e "${bold}Or specify a valid command as first argument:";
+    echo -e "Commands:";
+    echo -e "\t all   \t -- Run all models sequentially";
+    echo -e "\t clean \t -- Delete all *.stderr and *.stdout files";
+
+    # Exit with error
+    exit 1;
+fi
+
+# Trap ctrl-c and other exit signals and delete all temporary files
+trap TrapError 1 2 3 15;
+function TrapError() {
+    echo "Saliendo...";
+    exit;
+}
