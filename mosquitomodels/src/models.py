a3b88e54417b5e764604ae0eb65260334023f37c
Francisco Trucco <franciscoctrucco@gmail.com>
2017-08-28 20:10:48 -0300

    Delete positive models

--- mosquitomodels/src/models.py
+++ mosquitomodels/src/models.py
@@ -12,17 +12,7 @@ from sklearn.svm import SVR
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.linear_model import RidgeCV
 from sklearn.linear_model import LinearRegression
-import numpy as np
 from sklearn.tree import DecisionTreeRegressor
-
-
-def make_custom_model(model):
-    class CustomModel(model):
-        def predict(self, X):
-            y = super(CustomModel, self).predict(X)
-            y = np.maximum([0] * len(y), y)
-            return y
-    return CustomModel
 
 
 def add_pca_to_model(model):
@@ -31,22 +21,19 @@ def add_pca_to_model(model):
     return PCAModel
 
 
-mlpr = make_custom_model(MLPRegressor)
-
-
 def MLPRegressorProxy(alpha, neurons, layers):
     hidden_layer_sizes = tuple([neurons] * layers)
-    return mlpr(hidden_layer_sizes=hidden_layer_sizes, solver='lbfgs',
-                activation='logistic', alpha=alpha)
+    return MLPRegressor(hidden_layer_sizes=hidden_layer_sizes, solver='lbfgs',
+                        activation='logistic', alpha=alpha)
 
 
 MODELS = {
-    'dtr': make_custom_model(DecisionTreeRegressor),
-    'knnr': make_custom_model(KNeighborsRegressor),
+    'dtr': DecisionTreeRegressor,
+    'knnr': KNeighborsRegressor,
     'mlpr': MLPRegressorProxy,
-    'svr': make_custom_model(SVR),
-    'pcaknnr': add_pca_to_model(make_custom_model(KNeighborsRegressor)),
-    'pcadtr': add_pca_to_model(make_custom_model(DecisionTreeRegressor)),
-    'linear': make_custom_model(LinearRegression),
-    "ridge": make_custom_model(RidgeCV),
+    'svr': SVR,
+    'pcaknnr': add_pca_to_model(KNeighborsRegressor),
+    'pcadtr': add_pca_to_model(DecisionTreeRegressor),
+    'linear': LinearRegression,
+    "ridge": RidgeCV,
 }
